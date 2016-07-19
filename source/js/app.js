663c22a6a7e93e1377e79b940b28d4c52be5cb78
lapits <lapits@gmail.com>
2016-07-19 16:51:44 +0700

    autorization

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,20 @@
-(function() {
-  'use strict';
+'use strict';
 
+function autorization() {
+	var textBlock;
+  textBlock = document.getElementById("autorization-block");
+  if (textBlock!==null) {
+    textBlock.style.display = "none";
+  }
 
-})();
+  textBlock = document.getElementById("main-block");
+  if (textBlock!==null) {
+	  textBlock.style.transitionDelay = "2";
+	  textBlock.style.transitionDuration = "2s";
+	  textBlock.style.transitionTimingFunction = "linear";
+
+    textBlock.style.display = "block";
+
+  }
+
+};
