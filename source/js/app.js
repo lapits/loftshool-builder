8f4e08b3023cbe38bcf6f03765681518cf032721
lapits <lapits@gmail.com>
2016-07-23 09:25:19 +0700

    autorization JS

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,33 @@
 'use strict';
 
+function textBlockDisplaySetVisibility(textBlock, setVisible) {
+
+	if (setVisible) {
+		textBlock.style.display = "block";
+	}
+	else {
+		textBlock.style.display = "none";
+	}
+
+};
+
+
 function autorization() {
 	var textBlock;
+	var textMainBlock;
+
   textBlock = document.getElementById("animated-block");
   if (textBlock!==null) {
     textBlock.classList.toggle("wrap-animation-flip");
   }
 
+  textMainBlock = document.getElementById("main-block");
+  if (textMainBlock!==null) {
+  	if (textMainBlock.style.display === "none") {
+  		setTimeout(textBlockDisplaySetVisibility, 100, textMainBlock, true);
+  	}
+  	else {
+  		setTimeout(textBlockDisplaySetVisibility, 200, textMainBlock, false);
+  	}
+  }
 };
